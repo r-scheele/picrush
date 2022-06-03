172485db6b6a877c57dd283fc0c7325c035ec5af
r-scheele <abdulrahmanolamilekan88@gmail.com>
2022-06-03 21:40:48 +0100

    [Feature] worked on follow/unfollow/followers/followings feature

--- src/socials/socials.controller.ts
+++ src/socials/socials.controller.ts
@@ -1,21 +1,35 @@
-import { Controller, Put } from '@nestjs/common';
+import { Controller, Param, Put, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from 'src/guards/auth.guard';
 import { CurrentUser } from 'src/user/decorators/current-user.decorator';
 import { User } from 'src/user/user.entity';
 import { SocialsService } from './socials.service';
 
+
+@UseGuards(AuthGuard)
 @Controller()
 export class SocialsController {
     constructor(
         private socialsService: SocialsService,
     ) {}
 
-    @Put(':userId/follow')
-    async follow(userId: number, @CurrentUser() user: User) {
+    @Put('follow/:userId')
+    async follow(@Param('userId') userId: number, @CurrentUser() user: User) {
         return this.socialsService.follow(userId, user);
     }
 
-    @Put(':userId/unfollow')
-    async unfollow(userId: number, @CurrentUser() user: User) {
+    @Put('unfollow/:userId')
+    async unfollow(@Param('userId') userId: number, @CurrentUser() user: User) {
         return this.socialsService.unfollow(userId, user);
+    }
+
+
+    @Get('followers/:userId')
+    async followers(@Param('userId') userId: number, @CurrentUser() user: User) {
+        return this.socialsService.followers(userId, user);
+    }
+
+    @Get('followings/:userId')
+    async followings(@Param('userId') userId: number, @CurrentUser() user: User) {
+        return this.socialsService.followings(userId, user);
     }
 }
--- src/socials/socials.service.ts
+++ src/socials/socials.service.ts
@@ -1,5 +1,5 @@
-import { CurrentUser } from 'src/user/decorators/current-user.decorator';
-import { Injectable } from '@nestjs/common';
+
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/user/user.entity';
 import { Repository } from 'typeorm';
@@ -10,32 +10,64 @@ export class SocialsService {
 
 
     async follow(userId: number, currentUser: User) {
-        if (currentUser.id !== userId) {
-            const user = await this.userRepo.findOne(userId);
-            if (!user.followers.includes(currentUser.id)) {
-            user.followers.push(currentUser.id);
-            await this.userRepo.save(user);
 
-            const follower = await this.userRepo.findOne(currentUser.id);
-            follower.followings.push(user.id);
-            await this.userRepo.save(follower);
-        }    
-    }
+     
+        // you can't follow your own profile
+        if (currentUser.id === userId) throw new Error('You can\'t follow your own profile');
+        
+
+        const userToBeFollowed = await this.userRepo.findOne(userId);
+        const follower = await this.userRepo.findOne(currentUser.id);
+
+        // you can't follow a user that you already followed - if your id is not in their followers array
+        if (userToBeFollowed.followers.includes(follower.id)) throw new BadRequestException('You already follow this user');
+
+        // you can't follow a user that you already follow - if their id is not in your followings array   
+        if (follower.followings.includes(userToBeFollowed.id)) throw new BadRequestException('You already follow this user'); 
+        
+        userToBeFollowed.followers.push(follower.id);
+        follower.followings.push(userToBeFollowed.id);
+
+        await this.userRepo.save(userToBeFollowed);
+        await this.userRepo.save(follower);
+
+        return userToBeFollowed.followers
 }
 
 
 
-    async unfollow(userId: number, follower: User) {
-        if (follower.id !== userId) {
-            const user = await this.userRepo.findOne(userId);
-            if (user.followers.includes(follower.id)) {
-            user.followers = user.followers.filter(id => id !== follower.id);
-            await this.userRepo.save(user);
+    async unfollow(userId: number, currentUser: User) {
+             
+        // you can't follow your own profile
+        if (currentUser.id === userId) throw new Error('You can\'t unfollow your own profile');
+        
 
-            const unfollower = await this.userRepo.findOne(follower.id);
-            unfollower.followings = unfollower.followings.filter(id => id !== user.id);
-            await this.userRepo.save(unfollower);
-            }
-        } 
+        const userToBeUnFollowed = await this.userRepo.findOne(userId);
+        const follower = await this.userRepo.findOne(currentUser.id);
+
+        // you can't follow a user that you already followed - if your id is not in their followers array
+        if (!userToBeUnFollowed.followers.includes(follower.id)) throw new BadRequestException('You haven\'t followed this user');
+
+        // you can't follow a user that you already follow - if their id is not in your followings array   
+        if (!follower.followings.includes(userToBeUnFollowed.id)) throw new BadRequestException('You haven\'t follow this user'); 
+
+        userToBeUnFollowed.followers = userToBeUnFollowed.followers.filter(id => id !== follower.id);
+        follower.followings = follower.followings.filter(id => id !== userToBeUnFollowed.id);
+        
+        await this.userRepo.save(userToBeUnFollowed);
+        await this.userRepo.save(follower);
+
+
+        return userToBeUnFollowed.followers
+    }
+
+    async followers(userId: number, currentUser: User){
+        const user = await this.userRepo.findOne(userId);
+        return  this.userRepo.findByIds(user.followers)
+    }
+
+    async followings(userId: number, currentUser: User){
+        const user = await this.userRepo.findOne(userId);
+        return  this.userRepo.findByIds(user.followings)
     }
 }
